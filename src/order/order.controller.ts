b3115c20b388f8bf2222b9836372db05e9b3b610
Ramandeep <rdbhagat999@gmail.com>
2021-03-17 14:12:03 +0530

    adds export csv

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,6 +1,19 @@
-import {ClassSerializerInterceptor, Controller, Get, Query, UseGuards, UseInterceptors} from '@nestjs/common';
+import {
+    ClassSerializerInterceptor,
+    Controller,
+    Get,
+    Post,
+    Query,
+    Res,
+    UseGuards,
+    UseInterceptors
+} from '@nestjs/common';
 import {OrderService} from "./order.service";
 import {AuthGuard} from "../auth/auth.guard";
+import {Response} from 'express';
+import {parse} from "json2csv";
+import {Order} from "./models/order";
+import {OrderItem} from "./models/order-item";
 
 @UseInterceptors(ClassSerializerInterceptor)
 @UseGuards(AuthGuard)
@@ -10,6 +23,53 @@ export class OrderController {
 
     @Get()
     async all(@Query('page') page = 1) {
-        return this.orderService.paginate(page, 15, ['orderItems'])
+        return this.orderService.paginate(page, 15, ['orderItems']);
+    }
+
+    @Post('export-csv')
+    async export_csv(@Res() response: Response) {
+
+        const json = [];
+
+        const orders = await this.orderService.all(['orderItems']);
+
+        orders.forEach((order: Order) => {
+            json.push({
+                Id: order.id,
+                Name: order.name,
+                Email: order.email,
+                Product: '',
+                Price: '',
+                Quantity: '',
+            });
+
+            order.orderItems.forEach((item: OrderItem) => {
+                json.push({
+                    Id: '',
+                    Name: '',
+                    Email: '',
+                    Product: item.productTitle,
+                    Price: item.price,
+                    Quantity: item.quantity,
+                });
+            });
+        });
+
+        const fields = ['Id', 'Name', 'Email', 'Product', 'Price', 'Quantity'];
+        let csv = '';
+
+        try {
+            csv = parse(json, {fields});
+            console.log(csv);
+        } catch (err) {
+            csv = parse([], {fields});
+            console.error(err);
+        }
+
+        response.header('Content-type', 'text/csv');
+        response.attachment('orders.csv');
+
+        return response.send(csv);
+
     }
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -3,10 +3,49 @@ import {AbstractService} from "../common/abstract/abstract.service";
 import {InjectRepository} from "@nestjs/typeorm";
 import {Repository} from "typeorm";
 import {Order} from "./models/order";
+import {PaginatedResult} from "../common/paginated-result.interface";
 
 @Injectable()
 export class OrderService extends AbstractService {
     constructor(@InjectRepository(Order) private readonly orderRepository: Repository<Order>) {
         super(orderRepository);
+    }
+
+    async paginate(page = 1, take = 15, relations = []): Promise<PaginatedResult> {
+        if(! page) {
+            page = 1;
+        }
+
+        if(! take) {
+            take = 15;
+        }
+
+        const skip = (page - 1) * take;
+
+        const [rows, total] = await this.orderRepository.findAndCount({
+            take,
+            skip,
+            relations
+        });
+
+        const orders = rows.map((order) => ({
+            id: order.id,
+            name: order.name,
+            email: order.email,
+            createdAt: order.createdAt,
+            orderItems: order.orderItems,
+            total: order.total,
+        }));
+
+        return {
+            data: orders,
+            meta: {
+                total,
+                page,
+                take,
+                skip,
+                last_page: (page == 1 && total <= take) ? 1 : Math.ceil(total / take)
+            }
+        }
     }
 }
